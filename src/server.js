ec29de2fbc4fd7b0aaf519081d607ea7c0607711
Koshkarik <koshkarevj@mail.ru>
2018-03-18 17:50:53 +0300

    deploying to heroku

---
+++ src/server.js
@@ -0,0 +1,47 @@
+import express from 'express';
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/:time', (req, res) => {
+  const input = req.params.time;
+  let time;
+  const check = /^\d+$/.test(input);
+  switch (check) {
+    case true:
+      time = new Date(Number(input));
+      break;
+    default:
+      time = new Date(input);  
+  }
+  if(isNaN(time.getTime())) {
+    res.render('search', { humanDate: 'null', timeStamp: 'null' });
+    return;
+  }
+  const humanDate = time.toDateString();
+  const timeStamp = Date.parse(time);
+  res.render('search', { humanDate, timeStamp });
+});
+
+app.use((req, res) => {
+  res.status(404);
+  res.send('404 - Page was not found');
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500);
+  res.send('500 - Server Error');
+});
+
+
+app.listen(app.get('port'), () => {
+  console.log('Express server is running');
+});
